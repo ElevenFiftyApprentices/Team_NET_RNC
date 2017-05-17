c2f6ac8c37c5f64c0ffd13b6bce0511e7ef8cd55
Chris Sullivan <csullivan@KSMConsulting.com>
2017-05-17 15:22:22 -0400

    added a db context

--- ShoppingList.Data/ShoppingListItem.cs
+++ ShoppingList.Data/ShoppingListItem.cs
@@ -1,17 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ShoppingList.Data
 {
+
+    enum Priority
+    {
+        [Description("grab it now!")]
+        High,
+        [Description("Need it soon.")]
+        Medium,
+        [Description("It can wait.")]
+        Low
+    }
+
     class ShoppingListItem
     {
         public int ID { get; set; }
         public int ShoppingListID { get; set; }
         public string contents { get; set; }
         public bool IsChecked { get; set; }
+        public Priority Priority { get; set;  }
         public DateTimeOffset Created_Utc { get; set; }
         public DateTimeOffset Modified_Utc { get; set; }
     }
