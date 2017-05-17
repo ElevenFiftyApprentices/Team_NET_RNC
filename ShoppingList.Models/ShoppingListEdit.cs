ffbacb1150958c3fda264924c02755825c935d2b
Charles Michael <cmichaelgc@gmail.com>
2017-05-17 16:13:39 -0400

    made shopping list models

---
+++ ShoppingList.Models/ShoppingListEdit.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingList.Models
+{
+    class ShoppingListEdit
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Color { get; set; }
+    }
+}
